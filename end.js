8081e90bd26fa8c8952238f7a972853817b9ca25
Josemberg Farias Ferreira <josembergff@gmail.com>
2020-01-29 12:25:18 -0300

    ajustes de testes

--- end.js
+++ end.js
@@ -1,12 +1,13 @@
+
 const sb = new SendBird({ appId: "9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23" });
 
-
+var _groupChannel;
 sb.connect(`est-18826`, function (user, error) {
     if (error) {
         return;
     }
-    console.info("Usuário estabelecimento: ", user);
-
+    sb.updateCurrentUserInfo("Estabelecimento", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBO6Pxqa7Iv1zMKctTWsOHjNYdA7EMvP7W1Ll8fBlZc7tp5I-N&s");
+    aguardarMensagens();
     ///
     var channelListQuery = sb.GroupChannel.createMyGroupChannelListQuery();
     channelListQuery.includeEmpty = true;
@@ -37,12 +38,8 @@ sb.connect(`est-18826`, function (user, error) {
                     if (error) {
                         return;
                     }
-                    // console.info(groupChannel);
-                    // groupChannel.hide(true, true, function(response, error) {
-                    //     if (error) {
-                    //         return;
-                    //     }
-                    // });
+                    _groupChannel = groupChannel;
+                    carregarMensagens();
                 });
             });
         });
@@ -53,3 +50,77 @@ sb.connect(`est-18826`, function (user, error) {
     
 
 });
+
+const aguardarMensagens = () => {
+    const nomeHandler = `channelHandlerReceberMensagensGrupo_teste2`;
+    sb.removeChannelHandler(nomeHandler);
+    const ChannelHandler = new sb.ChannelHandler();
+
+    ChannelHandler.onMessageReceived = function (channel, message) {
+        adicionarMensagem(false, message);
+    };
+
+    sb.addChannelHandler(nomeHandler, ChannelHandler);
+};
+
+const adicionarMensagem = (usuarioAtual, message) => {
+    const novaLinhaMensagem = `${usuarioAtual ? "Usuário atual":"Outro usuário"} 
+    | Data: ${moment(new Date(message.createdAt)).format("lll")} 
+    | Nick:${message._sender ? message._sender.nickname : "sem nick"} 
+    | ${message.message} <br><br>`;
+    $("#divMensagens").prepend(novaLinhaMensagem);
+}
+
+const campoEnvioMensagemkeydown = (function (e) {
+    if (e.keyCode === 13 && !e.shiftKey) {
+        console.log(e.srcElement.value, this);
+        enviarMensagemEstabelecimento(e.srcElement.value);
+        e.srcElement.value = "";
+    }
+});
+
+const carregarMensagens = () => {
+    var prevMessageListQuery = _groupChannel.createPreviousMessageListQuery();
+    prevMessageListQuery.limit = 100;
+    prevMessageListQuery.reverse = false;
+
+    prevMessageListQuery.load(function (messages, error) {
+        if (error) {
+            return;
+        }
+
+        messages.forEach(f => {
+            adicionarMensagem(false, f);
+        });
+
+        _groupChannel.markAsRead();
+    });
+};
+
+const enviarMensagemEstabelecimento = (mensagem) => {
+    const params = new sb.UserMessageParams();
+
+    params.message = mensagem;
+    //params.customType = "";
+    params.data = JSON.stringify({ usuarios:{ "est-18826":{
+            Id: 144,
+            Nome: "Estabelecimento",
+            Email: "estabelecimento@gmail.com",
+            UrlImagemUsuario: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBO6Pxqa7Iv1zMKctTWsOHjNYdA7EMvP7W1Ll8fBlZc7tp5I-N&s"
+        } 
+    }});
+    params.mentionType = 'users';                       // Either 'users' or 'channel'
+    params.mentionedUserIds = ['est-18826'];        // Or mentionedUsers = Array<User>; 
+    //params.metaArrayKeys = ['linkTo', 'itemType'];
+    //params.translationTargetLanguages = ['fe', 'de'];   // French and German
+    params.pushNotificationDeliveryOption = 'default';  // Either 'default' or 'suppress' 
+
+    _groupChannel.sendUserMessage(params, function(message, error) {
+        if (error) {
+            console.info("Erro Mensagem enviada", error);
+            return;
+        }
+        adicionarMensagem(true, message);
+    });
+
+}
--- start.js
+++ start.js
@@ -1,57 +1,98 @@
 const sb = new SendBird({ appId: "9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23" });
 
-
+var _groupChannel;
     sb.connect(`user-144`, function (user, error) {
         if (error) {
             return;
         }
-        console.info("Usuário cliente: ", user);
-        sb.updateCurrentUserInfo("Alex Ionic", "https://kaddeqas.blob.core.windows.net/imagemusuarios/144_thumbnail.jpg?t=637158067723869529");
+        aguardarMensagens();
+        sb.updateCurrentUserInfo("Alex Ionic", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-EccM8blle2lfzwlPidvvAp2gqfOpDfvaq76sUSyqlDdP1hgcAg&s");
         let userIdsGroup = ['user-144', 'est-18826'];
         let NAME = "user-144-est-18826";
+
         sb.GroupChannel.createChannelWithUserIds(userIdsGroup, true, NAME, null, null, function (groupChannel, error) {
             if (error) {
-                console.log("Erro usuários cliente: ", error);
+                console.error("Erro usuários cliente: ", error);
                 return;
             }
 
-            console.info("Grupos cliente: ", groupChannel);
+            _groupChannel = groupChannel;
             
-            var userIds = ['user-144', 'est-18826'];
-
-            groupChannel.inviteWithUserIds(userIds, function(response, error) {
-                if (error) {
-                    return;
-                }
-            });
+            carregarMensagens();
             
-            ////
-            const params = new sb.UserMessageParams();
-
-            params.message = "Olá";
-            //params.customType = "";
-            params.data = JSON.stringify({ usuarios:{ "user-144":{
-                    Id: 144,
-                    Nome: "Alex",
-                    Email: "alex@gmail.com",
-                    UrlImagemUsuario: "https://kaddeqas.blob.core.windows.net/imagemusuarios/144_thumbnail.jpg?t=637158067723869529"
-                } 
-            }});
-            params.mentionType = 'users';                       // Either 'users' or 'channel'
-            params.mentionedUserIds = ['est-18826'];        // Or mentionedUsers = Array<User>; 
-            //params.metaArrayKeys = ['linkTo', 'itemType'];
-            //params.translationTargetLanguages = ['fe', 'de'];   // French and German
-            params.pushNotificationDeliveryOption = 'default';  // Either 'default' or 'suppress' 
-
-            groupChannel.sendUserMessage(params, function(message, error) {
-                if (error) {
-                    console.info("Erro Mensagem enviada", error);
-                    return;
-                }
-
-                console.info("Mensagem enviada", message);
-            });
-
 
         });
     });
+
+    const aguardarMensagens = () => {
+        const nomeHandler = `channelHandlerReceberMensagensGrupo_teste1`;
+        sb.removeChannelHandler(nomeHandler);
+        const ChannelHandler = new sb.ChannelHandler();
+    
+        ChannelHandler.onMessageReceived = function (channel, message) {
+            adicionarMensagem(false, message);
+        };
+    
+        sb.addChannelHandler(nomeHandler, ChannelHandler);
+    };
+
+    const adicionarMensagem = (usuarioAtual, message) => {
+        const novaLinhaMensagem = `${usuarioAtual ? "Usuário atual":"Outro usuário"} | Data: ${moment(new Date(message.createdAt)).format("lll")} | Nick:${message._sender.nickname} | ${message.message} <br><br>`;
+        $("#divMensagens").prepend(novaLinhaMensagem);
+    }
+
+    const campoEnvioMensagemkeydown = (function (e) {
+        if (e.keyCode === 13 && !e.shiftKey) {
+            console.log(e.srcElement.value, this);
+            enviarMensagemCliente(e.srcElement.value);
+            e.srcElement.value = "";
+        }
+    });
+
+    const carregarMensagens = () => {
+        var prevMessageListQuery = _groupChannel.createPreviousMessageListQuery();
+        prevMessageListQuery.limit = 100;
+        prevMessageListQuery.reverse = false;
+    
+        prevMessageListQuery.load(function (messages, error) {
+            if (error) {
+                return;
+            }
+    
+            messages.forEach(f => {
+                adicionarMensagem(false, f);
+            });
+    
+            _groupChannel.markAsRead();
+        });
+    };
+
+    const enviarMensagemCliente = (mensagem) => {
+        const params = new sb.UserMessageParams();
+
+        params.message = mensagem;
+        //params.customType = "";
+        params.data = JSON.stringify({ usuarios:{ "user-144":{
+                Id: 144,
+                Nome: "Alex",
+                Email: "alex@gmail.com",
+                UrlImagemUsuario: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-EccM8blle2lfzwlPidvvAp2gqfOpDfvaq76sUSyqlDdP1hgcAg&s"
+            } 
+        }});
+        params.mentionType = 'users';                       // Either 'users' or 'channel'
+        params.mentionedUserIds = ['est-18826'];        // Or mentionedUsers = Array<User>; 
+        //params.metaArrayKeys = ['linkTo', 'itemType'];
+        //params.translationTargetLanguages = ['fe', 'de'];   // French and German
+        params.pushNotificationDeliveryOption = 'default';  // Either 'default' or 'suppress' 
+
+        _groupChannel.sendUserMessage(params, function(message, error) {
+            if (error) {
+                console.info("Erro Mensagem enviada", error);
+                return;
+            }
+            adicionarMensagem(true, message);
+        });
+
+    }
+
+    
